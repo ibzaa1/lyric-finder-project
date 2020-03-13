6633b10c12387efa1f97e7bbe5619bdb981a8f53
IbrahimT <ibrahim.thaanbir@gmail.com>
2020-03-13 22:23:11 +0000

    my lyric finder project

---
+++ script.js
@@ -0,0 +1,106 @@
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const result = document.getElementById('result')
+const more = document.getElementById('more')
+
+const apiURL = 'https://api.lyrics.ovh';
+
+// Search song name or artist 
+async function searchSongs(term) {
+    const res = await fetch(`${apiURL}/suggest/${term}`);
+    const data = await res.json();
+
+    showData(data);
+}
+
+// Show artist and song 
+
+function showData(data) {
+   
+    /*  
+    let output = '';
+
+    data.data.forEach(song => {
+        output += `
+        <li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}
+        </span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>
+        `;
+    });
+
+    result.innerHTML = `
+    <ul class="songs">
+        ${output}
+    </ul>
+    `; 
+    */
+
+    result.innerHTML = `
+    <ul class=songs>
+    ${data.data.map(song => `  <li>
+    <span><strong>${song.artist.name}</strong> - ${song.title}
+    </span>
+    <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    </li>`)
+    .join('')
+}
+    </ul>
+    `;
+
+    if (data.prev || data.next) {
+        more.innerHTML =`
+        ${data.prev ? `<button class=btn onclick="getMoreSongs('${data.prev}')">Previous</button>`: ''}
+        ${data.next ? `<button class=btn onclick="getMoreSongs('${data.next}')">Next</button>`: ''}
+        `;
+    } else {
+        more.innerHTML = '';
+    }
+}
+
+// Get previous and next set of songs   
+async function getMoreSongs(url) {
+    const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = await res.json();
+
+    showData(data);
+}
+
+// Get lyrics
+async function getLyrics(artist, songTitle) {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle} </h2> <span>${lyrics}</span>`;
+    
+    more.innerHTML = '';
+}
+
+// Event listener
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const searchTerm = search.value.trim();
+
+    if(!searchTerm) {
+        alert('Please type in a song name or artist');
+    } else {
+        searchSongs(searchTerm);
+    }
+});
+
+// Get Lyrics Button
+result.addEventListener('click', e =>{
+    const clickedElement = e.target;
+
+    if(clickedElement.tagName === 'BUTTON') {
+        const artist = clickedElement.getAttribute('data-artist');
+        const songTitle = clickedElement.getAttribute('data-songtitle');
+    
+        getLyrics(artist, songTitle);
+
+    }
+});
